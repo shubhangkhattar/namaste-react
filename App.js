60d6fe963c293bd409055a3df3c2f31eee699000
shubhang khattar <shubhang1995khattar@gmail.com>
2023-12-26 10:29:02 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,8 @@
+
+const parent = React.createElement("div",{id : "parent"}, 
+    React.createElement("div",{id : "child"}, 
+    React.createElement("h1",{}, "Hello World from React!!!!!!") ));
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent);
